00bac89bcecc5c8db2510c8ef83bfe6e34d52e92
Nerielle <nelya.iva@gmail.com>
2015-07-09 21:36:24 +0300

    FirstSample

---
+++ RabbitMQFirstSample/Sources/RabbitMQConsumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQConsumer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory {HostName = "localhost"};
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    var consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume("hello", true, consumer);
+
+                    Console.WriteLine(" [*] Waiting for messages." +
+                                      "To exit press CTRL+C");
+                    while (true)
+                    {
+                        BasicDeliverEventArgs ea = consumer.Queue.Dequeue();
+
+                        byte[] body = ea.Body;
+                        string message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQFirstSample/Sources/RabbitMQProducer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQProducer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory {HostName = "localhost"};
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    string message = "Hello World!";
+                    byte[] body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", "hello", null, body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
